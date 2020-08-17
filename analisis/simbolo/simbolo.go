03dc8b247c182f0fb5b6bc2ade3f19b3e04f7413
Didier Domínguez <dadu0699@hotmail.com>
2020-08-17 17:57:25 -0600

    [ADD] Main

---
+++ analisis/simbolo/simbolo.go
@@ -0,0 +1,8 @@
+package simbolo
+
+import "fmt"
+
+// Funcion simbolo
+func Simbolo() {
+	fmt.Println("Simbolo")
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"Sistema-de-archivos-LWH/analisis/simbolo"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("main")
+	simbolo.Simbolo()
+}
